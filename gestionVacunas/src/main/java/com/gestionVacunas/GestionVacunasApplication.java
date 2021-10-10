a8e7c55138f11afbe9a2e4e3d53e2d3cc015d9be
Fernando <gatoguevara2@hotmail.com>
2021-10-10 15:58:46 -0500

    Creacion del Proyecto

---
+++ gestionVacunas/src/main/java/com/gestionVacunas/GestionVacunasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionVacunas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVacunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVacunasApplication.class, args);
+	}
+
+}
---
+++ gestionVacunas/src/test/java/com/gestionVacunas/GestionVacunasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionVacunas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVacunasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
